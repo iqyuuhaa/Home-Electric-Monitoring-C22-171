5f00df49982b417998654b177aa8d8f3af123a47
iQyuuHaaa <ibnuqomarilhuda@gmail.com>
2022-12-10 19:40:40 +0700

    Finishing apps

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,12 @@
-const HomePage = () => {}
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+
+const HomePage = () => {
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        navigate("/login")
+    }, [navigate])
+}
 
 export default HomePage
